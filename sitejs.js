e0a24050a68ac8bf8c6c8555b1e86b0d3ceb67e3
elialdinger <49462755+elialdinger@users.noreply.github.com>
2020-01-04 11:12:40 -0700

    changed files

---
+++ sitejs.js
@@ -0,0 +1,34 @@
+function resizeIframe(obj) {
+	obj.style.height = 0;
+	obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+	obj.style.width = 0;
+	obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
+}
+
+function cashtext() {
+	//var about = document.getElementById('about');
+	//var links = document.getElementById('links');
+	var rand = Math.floor(Math.random() * (7 - 1) + 1);
+	switch(rand)
+		{
+			case(1):
+				document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + "$$$$$$$$";
+				break
+			case(2):
+				document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + "$$";
+				break
+			case(3):
+				document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + "$$$$$$$";
+				break
+			case(4):
+				document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + "$$$";
+				break
+			case(5):
+				document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + "$$$$$$";
+				break
+			default:
+				document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + "$$$$$";
+				break
+
+		}
+}
